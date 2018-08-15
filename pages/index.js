aba5b692e2f45551c5138b26436b7528e6538805
chenshijie <cruoru@gmail.com>
2018-08-15 18:22:06 +0800

    fix get data

--- pages/index.js
+++ pages/index.js
@@ -2,21 +2,24 @@ import Layout from './components/MyLayout.js'
 import Link from 'next/link'
 import fetch from 'isomorphic-unfetch'
 
-const Index = (props) => (
-  <Layout>
-    <h1>Batman TV Shows</h1>
-  </Layout>
-)
+const Index = (props) => {
+  console.log(props)
+  return (
+    <Layout>
+      <h1>Batman TV Shows</h1>
+    </Layout>
+  );
+}
 
-// Index.getInitialProps = async function() {
-//   const res = await fetch('https://api.tvmaze.com/search/shows?q=batman')
-//   const data = await res.json()
+Index.getInitialProps = async function() {
+  const res = await fetch('https://api.tvmaze.com/search/shows?q=batman')
+  const data = await res.json()
 
-//   console.log(`Show data fetched. Count: ${data.length}`)
+  console.log(`Show data fetched. Count: ${data.length}`)
 
-//   return {
-//     shows: data
-//   }
-// }
+  return {
+    shows: data
+  }
+}
 
 export default Index
