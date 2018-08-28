60570f20e1d5283e79388f37977deddc1f61520d
chenshijie <cruoru@gmail.com>
2018-08-28 15:09:14 +0800

    demo finish

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,78 @@
 import Layout from './components/MyLayout.js'
 import Link from 'next/link'
 import fetch from 'isomorphic-unfetch'
+import Markdown from 'react-markdown'
 
 const Index = (props) => {
-  console.log(props)
   return (
     <Layout>
       <h1>Batman TV Shows</h1>
+      <Markdown source={`
+          This is our blog post.
+          Yes. We can have a [link](/link).
+          And we can have a title as well.
+
+          ### This is a title
+
+          And here's the content.
+        `} />
+      <ul>
+        {props.shows.map(({ show }) => (
+          <li key={show.id}>
+            <Link as={`/p/${show.id}`} href={`/post?id=${show.id}`}>
+              <a>{show.name}</a>
+            </Link>
+          </li>
+        ))}
+      </ul>
+      <style jsx>{`
+      h1, a {
+        font-family: "Arial";
+      }
+
+      ul {
+        padding: 0;
+      }
+
+      li {
+        list-style: none;
+        margin: 5px 0;
+      }
+
+      a {
+        text-decoration: none;
+        color: blue;
+      }
+
+      a:hover {
+        opacity: 0.6;
+      }
+
+      .markdown {
+        font-family: 'Arial';
+      }
+ 
+      .markdown a {
+        text-decoration: none;
+        color: blue;
+      }
+ 
+      .markdown a:hover {
+        opacity: 0.6;
+      }
+ 
+      .markdown h3 {
+        margin: 0;
+        padding: 0;
+        text-transform: uppercase;
+      }
+    `}</style>
+
     </Layout>
   );
 }
 
-Index.getInitialProps = async function() {
+Index.getInitialProps = async function () {
   const res = await fetch('https://api.tvmaze.com/search/shows?q=batman')
   const data = await res.json()
 
--- server.js
+++ server.js
@@ -5,13 +5,12 @@ const dev = process.env.NODE_ENV !== 'production'
 const app = next({ dev })
 const handle = app.getRequestHandler()
 
-app.prepare()
-.then(() => {
+app.prepare().then(() => {
   const server = express()
 
   server.get('/p/:id', (req, res) => {
     const actualPage = '/post'
-    const queryParams = { title: req.params.id } 
+    const queryParams = { title: req.params.id }
     app.render(req, res, actualPage, queryParams)
   })
 
@@ -23,8 +22,7 @@ app.prepare()
     if (err) throw err
     console.log('> Ready on http://localhost:3000')
   })
-})
-.catch((ex) => {
+}).catch((ex) => {
   console.error(ex.stack)
   process.exit(1)
 })
