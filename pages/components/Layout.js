5b0460974705681a894038a46fb8ff40b02bb213
ruoru <cruoru@gmail.com>
2018-09-08 23:17:08 +0800

    1

--- pages/components/Layout.js
+++ pages/components/Layout.js
@@ -1,16 +1,15 @@
-import Header from './Header'
+import Header from "./Header";
 
-const layoutStyle = {
-  margin: 20,
-  padding: 20,
-  border: '1px solid #DDD'
-}
-
-const Layout = (props) => (
-  <div style={layoutStyle}>
+const Layout = props => (
+  <div className="app">
+    <link
+      rel="stylesheet"
+      type="text/css"
+      href="https://cdnjs.cloudflare.com/ajax/libs/antd/3.9.1/antd.min.css"
+    />
     <Header />
     {props.children}
   </div>
-)
+);
 
-export default Layout
+export default Layout;
--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,4 @@
-import Layout from "./components/MyLayout.js";
+import Layout from "./components/Layout.js";
 import Link from "next/link";
 import fetch from "isomorphic-unfetch";
 import Markdown from "react-markdown";
